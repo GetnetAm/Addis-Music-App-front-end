722d4f431caf8e5700971de3e44b9d6f8d07cebc
GetnetAm <getnetamsalu2119@gmail.com>
2024-08-20 18:06:50 -0700

    home loading page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,23 @@
 import { css } from "@emotion/react";
 import Banner from "./Banner";
 import AduioList from "../components/AduioList";
+const {loading} = useSelector(state=> state.data);
 
 const mainContainer = css`
   width: 100%;
   height: 100%;
 `;
+
+if (loading) {
+  return <div className="loading">
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+</div>;
+}
 
 function Home() {
   return (
